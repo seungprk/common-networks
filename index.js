8a13020da36750bcc5cfb037a9f89aaa7d214de8
seungprk@gmail.com <seungprk@gmail.com>
2018-09-18 09:24:42 -0700

    Show whether files are directories or not using promises

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // start from current directly and recursively list all files
+const util = require('util');
+const fs = require('fs');
+const path = require('path');
+
+const lstat = util.promisify(fs.lstat);
+const readdir = util.promisify(fs.readdir);
+
+readdir(__dirname)
+  .then((files) => {
+    files.forEach((file) => {
+      lstat(file)
+        .then(stats => console.log(stats.isDirectory()));
+    });
+  });
