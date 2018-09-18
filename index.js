ebe41b6024fd22ebbfce1334fb4f3e09e081f828
seungprk@gmail.com <seungprk@gmail.com>
2018-09-18 09:46:43 -0700

    Recursively print full filepath names

--- index.js
+++ index.js
@@ -6,10 +6,22 @@ const path = require('path');
 const lstat = util.promisify(fs.lstat);
 const readdir = util.promisify(fs.readdir);
 
-readdir(__dirname)
-  .then((files) => {
-    files.forEach((file) => {
-      lstat(file)
-        .then(stats => console.log(stats.isDirectory()));
+const listDirectories = (directory) => {
+  readdir(directory)
+    .then((files) => {
+      files.forEach((file) => {
+        const filePath = path.join(directory, file);
+        lstat(filePath)
+          .then((stats) => {
+            if (stats.isDirectory()) {
+              const innerPath = path.join(directory, file);
+              listDirectories(innerPath);
+            } else {
+              console.log(filePath);
+            }
+          });
+      });
     });
-  });
+};
+
+listDirectories(__dirname);
