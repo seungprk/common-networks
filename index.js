451b95c1788de4b5cb1f556765878a1b918661df
seungprk@gmail.com <seungprk@gmail.com>
2018-09-18 09:59:19 -0700

    Change to sync fs calls to print files in depth first

--- index.js
+++ index.js
@@ -1,27 +1,18 @@
-// start from current directly and recursively list all files
-const util = require('util');
 const fs = require('fs');
 const path = require('path');
 
-const lstat = util.promisify(fs.lstat);
-const readdir = util.promisify(fs.readdir);
-
 const listDirectories = (directory) => {
-  readdir(directory)
-    .then((files) => {
-      files.forEach((file) => {
-        const filePath = path.join(directory, file);
-        lstat(filePath)
-          .then((stats) => {
-            if (stats.isDirectory()) {
-              const innerPath = path.join(directory, file);
-              listDirectories(innerPath);
-            } else {
-              console.log(filePath);
-            }
-          });
-      });
-    });
+  const files = fs.readdirSync(directory);
+  files.forEach((file) => {
+    const filePath = path.join(directory, file);
+    const stats = fs.lstatSync(filePath);
+    if (stats.isDirectory()) {
+      const innerPath = path.join(directory, file);
+      listDirectories(innerPath);
+    } else {
+      console.log(filePath);
+    }
+  });
 };
 
 listDirectories(__dirname);
